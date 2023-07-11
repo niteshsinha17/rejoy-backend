36a7f2a6383bed1c40bcef3c7a3005ce4bfe130c
Nitesh Sinha <niteshsinha@Niteshs-MacBook-Pro.local>
2023-07-11 20:29:45 +0530

    build created

---
+++ build/icons/index.ts
@@ -0,0 +1,35 @@
+import academyIcon from "./academy.svg";
+import arrowDownIcon from "./arrow-down.svg";
+import CloseIcon from "./close.svg";
+import facebookIcon from "./facebook.svg";
+import houseCallIcon from "./house-call.svg";
+import instagramIcon from "./instagram.svg";
+import linkedInIcon from "./linkedin.svg";
+import MenuIcon from "./menu.svg";
+import motionIcon from "./motion.svg";
+import moveIcon from "./move.svg";
+import musculoskeletalIcon from "./musculoskeletal.svg";
+import onCallIcon from "./on-call.svg";
+import therapyIcon from "./therapy.svg";
+import tiktokIcon from "./tiktok.svg";
+import twitterIcon from "./twitter.svg";
+import youTubeIcon from "./youtube.svg";
+
+export {
+  CloseIcon,
+  MenuIcon,
+  academyIcon,
+  arrowDownIcon,
+  facebookIcon,
+  houseCallIcon,
+  instagramIcon,
+  linkedInIcon,
+  motionIcon,
+  moveIcon,
+  musculoskeletalIcon,
+  onCallIcon,
+  therapyIcon,
+  tiktokIcon,
+  twitterIcon,
+  youTubeIcon,
+};
