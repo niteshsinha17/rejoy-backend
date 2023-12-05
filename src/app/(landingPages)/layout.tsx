9b826ea19717164d429c84de9487ddc9f4feeeb4
Rituraj <rituraj.tc@gmail.com>
2023-12-04 23:05:09 -0500

    Some text

--- src/app/(landingPages)/layout.tsx
+++ src/app/(landingPages)/layout.tsx
@@ -5,8 +5,8 @@ import Footer from "./_components/footer";
 import "./global.css";
 
 export const metadata = {
-  title: "Physical Therapy: Anytime, Anywhere",
-  description: "Physical Therapy: Anytime, Anywhere",
+  title: "Back and Joint Pain Care with Artificial Intelligence",
+  description: "Back and Joint Pain Care with Artificial Intelligence",
 };
 
 export default function RootLayout(props: ILayoutProps) {
@@ -14,7 +14,6 @@ export default function RootLayout(props: ILayoutProps) {
     <html lang="en" className="text-sm sm:text-lg">
       <body suppressHydrationWarning={true}>
         <GaProvider></GaProvider>
-
         <LandingPageHeader />
         {props.children}
         <Footer />
