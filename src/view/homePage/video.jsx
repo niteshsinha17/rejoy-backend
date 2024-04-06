e59a80dd199fd6ce95a20ebedcb7679ee7c1eb9f
Nitesh Sinha <niteshsinha1707@gmail.com>
2024-04-06 23:01:36 +0530

    Changed video url

--- src/view/homePage/video.jsx
+++ src/view/homePage/video.jsx
@@ -10,7 +10,7 @@ const Video = () => {
         <div className="pb-[56.25%] relative overflow-hidden">
           <iframe
             className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/Y4HHGzZ08JE"
+            src="https://www.youtube.com/embed/pCo5vtkRhL0"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
